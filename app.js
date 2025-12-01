c393d465adc800d74755760952d93482df77a50f
Leo Santander Nycz <121593322+SantanderNycz@users.noreply.github.com>
2025-12-01 10:17:19 +0000

    reupdate backend

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+// DB Connection
+const conn = require("./db/conn");
+
+conn();
+
+// Routes
+const routes = require("./routes/router");
+
+app.use("/api", routes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server online on port ${PORT}`);
+});
---
+++ db/conn.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Conectado ao MongoDB"))
+  .catch((err) => console.log(err));
+
+const mongoose = require("mongoose");
+
+async function main() {
+  try {
+    mongoose.set("strictQuery", true);
+
+    await mongoose.connect(
+      "mongodb+srv://santandernycz:S7TB8OUYUFGDN4Bm@cluster0.rvhc5n1.mongodb.net/?appName=Cluster0"
+    );
+    console.log("Database connected");
+  } catch (error) {
+    console.log(`Error ${error}`);
+  }
+}
+
+module.exports = main;
